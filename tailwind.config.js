3fcd908149983f4b6fb7e3aa0f5f0f55908ea8c5
ShriramJayanth <csshriramjayanth.cse2022@citchennai.net>
2024-02-05 14:59:16 +0530

    Font family added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import { DefaultTheme } from 'tailwindcss/defaultTheme';
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        MainFont: ['Inter var', ...DefaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
