fc511a1b732da9bbc919191d1f160b2637793894
ManikavelP <manikavelp.cse2022@citchennai.net>
2024-03-04 20:27:35 +0530

    ourTeam

--- src/pages/MyBookingsPage/MyBookingsPage.jsx
+++ src/pages/MyBookingsPage/MyBookingsPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
 const MyBookingsPage = () => {
   return (
-    <div>MyBookingsPage</div>
+    <div className='w-full h-screen bg-backGround'>
+      <Navbar booking = "true"/>
+
+      <h1>MyBookings</h1>
+
+
+
+
+      <Footer/>
+
+      
+    </div>
   )
 }
 
